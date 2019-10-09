1c764db5aab378ef05f662d41ccb90456b7edc97
Terry Chia <terrycwk1994@gmail.com>
2019-10-09 17:37:18 +0800

    Handle overloaded native functions.

--- JNIAnalyzer/ghidra_scripts/JNIAnalyzer.java
+++ JNIAnalyzer/ghidra_scripts/JNIAnalyzer.java
@@ -39,6 +39,7 @@ public class JNIAnalyzer extends GhidraScript {
 
 	private class MethodInformation {
 		private String methodName;
+		private String argumentSignature;
 		private ArrayList<String> argumentTypes;
 		private String returnType;
 		private boolean isStatic;
@@ -59,6 +60,8 @@ public class JNIAnalyzer extends GhidraScript {
 
 		NativeMethodsList methodsList = gson.fromJson(reader, NativeMethodsList.class);
 
+		// Iterate through all functions in the binary and look for the ones starting
+		// with "Java_". Ignore the functions marked with "JNIAnalyzer:IGNORE".
 		println("[+] Enumerating JNI functions...");
 		HashMap<String, Function> functions = new HashMap<String, Function>();
 		ArrayList<String> ignoredFunctions = new ArrayList<String>();
@@ -89,42 +92,74 @@ public class JNIAnalyzer extends GhidraScript {
 		}
 		println();
 
-		println("[+] Applying function signatures...");
+		// Bucket the Methods parsed from the JSON based on the method name.
+		// This will allow us to determine if we need to use the overloaded
+		// form the name mangling.
+		HashMap<String, ArrayList<MethodInformation>> methodMap = new HashMap<String, ArrayList<MethodInformation>>();
+
 		for (MethodInformation method : methodsList.methods) {
-
-			String methodName = this.generateNativeMethodName(method, false);
-
-			if (functions.containsKey(methodName)) {
-				Function f = functions.get(methodName);
-
-				Parameter[] params = new Parameter[method.argumentTypes.size() + 2]; // + 2 to accomodate env and thiz
-
-				params[0] = new ParameterImpl("env", this.manager.getDataType("/jni_all.h/JNIEnv *"),
-						this.currentProgram, SourceType.USER_DEFINED);
-
-				if (method.isStatic) {
-					params[1] = new ParameterImpl("thiz", this.manager.getDataType("/jni_all.h/jclass"),
-							this.currentProgram, SourceType.USER_DEFINED);
-				} else {
-					params[1] = new ParameterImpl("thiz", this.manager.getDataType("/jni_all.h/jobject"),
-							this.currentProgram, SourceType.USER_DEFINED);
-				}
-
-				for (int i = 0; i < method.argumentTypes.size(); i++) {
-					String argType = method.argumentTypes.get(i);
-
-					params[i + 2] = new ParameterImpl("a" + String.valueOf(i),
-							this.manager.getDataType("/jni_all.h/" + argType), this.currentProgram,
-							SourceType.USER_DEFINED);
-				}
-
-				Parameter returnType = new ReturnParameterImpl(
-						this.manager.getDataType("/jni_all.h/" + method.returnType), this.currentProgram);
-
-				f.updateFunction(null, returnType, Function.FunctionUpdateType.DYNAMIC_STORAGE_FORMAL_PARAMS, true,
-						SourceType.USER_DEFINED, params);
+			if (methodMap.containsKey(method.methodName)) {
+				methodMap.get(method.methodName).add(method);
+			} else {
+				ArrayList<MethodInformation> t = new ArrayList<MethodInformation>();
+				t.add(method);
+				methodMap.put(method.methodName, t);
 			}
 		}
+
+		println("[+] Applying function signatures...");
+		for (String m : methodMap.keySet()) {
+			ArrayList<MethodInformation> methodList = methodMap.get(m);
+
+			if (methodList.size() == 1) {
+				MethodInformation method = methodList.get(0);
+				String methodName = this.generateNativeMethodName(method, false);
+
+				if (functions.containsKey(methodName)) {
+					Function f = functions.get(methodName);
+					this.applyFunctionSignature(methodName, method, f);
+				}
+			} else {
+				for (MethodInformation method : methodList) {
+					String methodName = this.generateNativeMethodName(method, true);
+					if (functions.containsKey(methodName)) {
+						Function f = functions.get(methodName);
+						this.applyFunctionSignature(methodName, method, f);
+					}
+				}
+			}
+		}
+	}
+
+	private void applyFunctionSignature(String methodName, MethodInformation method, Function f)
+			throws InvalidInputException, DuplicateNameException {
+
+		Parameter[] params = new Parameter[method.argumentTypes.size() + 2]; // + 2 to accomodate env and thiz
+
+		params[0] = new ParameterImpl("env", this.manager.getDataType("/jni_all.h/JNIEnv *"), this.currentProgram,
+				SourceType.USER_DEFINED);
+
+		if (method.isStatic) {
+			params[1] = new ParameterImpl("thiz", this.manager.getDataType("/jni_all.h/jclass"), this.currentProgram,
+					SourceType.USER_DEFINED);
+		} else {
+			params[1] = new ParameterImpl("thiz", this.manager.getDataType("/jni_all.h/jobject"), this.currentProgram,
+					SourceType.USER_DEFINED);
+		}
+
+		for (int i = 0; i < method.argumentTypes.size(); i++) {
+			String argType = method.argumentTypes.get(i);
+
+			params[i + 2] = new ParameterImpl("a" + String.valueOf(i),
+					this.manager.getDataType("/jni_all.h/" + argType), this.currentProgram, SourceType.USER_DEFINED);
+		}
+
+		Parameter returnType = new ReturnParameterImpl(this.manager.getDataType("/jni_all.h/" + method.returnType),
+				this.currentProgram);
+
+		f.updateFunction(null, returnType, Function.FunctionUpdateType.DYNAMIC_STORAGE_FORMAL_PARAMS, true,
+				SourceType.USER_DEFINED, params);
+
 	}
 
 	private DataTypeManager getDataTypeManageFromArchiveFile() throws IOException, DuplicateIdException {
@@ -157,12 +192,33 @@ public class JNIAnalyzer extends GhidraScript {
 
 	private String generateNativeMethodName(MethodInformation methodInfo, boolean isOverloaded) {
 		String methodName = methodInfo.methodName;
-		methodName = methodName.replaceAll("_", "_1");
 
+		methodName = methodName.replaceAll("_", "_1");
+		methodName = this.mangleUnicode(methodName);
+
+		String[] methodNameSplit = methodName.toString().split("\\.");
+		methodName = "Java_" + String.join("_", methodNameSplit);
+
+		if (isOverloaded) {
+			String argumentSignature = methodInfo.argumentSignature;
+
+			argumentSignature = argumentSignature.replaceAll("_", "_1");
+			argumentSignature = argumentSignature.replaceAll(";", "_2");
+			argumentSignature = argumentSignature.replaceAll("\\[", "_3");
+			argumentSignature = this.mangleUnicode(argumentSignature);
+			argumentSignature = argumentSignature.replaceAll("/", "_");
+
+			methodName = methodName + "__" + argumentSignature;
+		}
+
+		return methodName;
+	}
+
+	private String mangleUnicode(String s) {
 		StringBuilder sb = new StringBuilder();
 
-		for (int offset = 0; offset < methodName.length();) {
-			int codepoint = methodName.codePointAt(offset);
+		for (int offset = 0; offset < s.length();) {
+			int codepoint = s.codePointAt(offset);
 
 			// If codepoint is ASCII:
 			if (codepoint >= 0 && codepoint <= 127) {
@@ -176,15 +232,6 @@ public class JNIAnalyzer extends GhidraScript {
 			offset += Character.charCount(codepoint);
 		}
 
-		String[] methodNameSplit = sb.toString().split("\\.");
-
-		if (isOverloaded) {
-			println("generateNativeMethodName - isOverloaded not implemented yet.");
-			return null;
-		}
-
-		methodName = "Java_" + String.join("_", methodNameSplit);
-		println("[-] " + methodName);
-		return methodName;
+		return sb.toString();
 	}
 }
